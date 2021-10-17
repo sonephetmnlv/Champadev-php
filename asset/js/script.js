0f304e4551c1b9750b7167d2b7ebd023581ddae5
sonephetmnlv <sonephetmnlv@gmail.com>
2021-10-17 11:54:47 +0700

    responsive

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,4 +1,31 @@
-function open_link() {
     let link = document.getElementById('links');
-    link.classList.toggle('d-flex');
-}
+    let ham_open = document.getElementById('ham-open');
+    let ham_close = document.getElementById('ham-close');
+
+    function open_link() {
+        ham_open.classList.add('d-none');
+        ham_close.classList.remove('d-none');
+        link.classList.remove('d-none');
+    }
+
+    function close_link() {
+        ham_open.classList.remove('d-none');
+        ham_close.classList.add('d-none');
+        link.classList.add('d-none');
+    }
+
+
+
+
+    window.onscroll = function() { myFunction() };
+
+    var navbar = document.getElementById("nav-bar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            navbar.classList.add("nav-bar")
+        } else {
+            navbar.classList.remove("nav-bar");
+        }
+    }
