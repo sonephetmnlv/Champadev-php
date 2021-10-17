fb3f1805f3ff774d6957dd3961bccc7196191947
sonephetmnlv <sonephetmnlv@gmail.com>
2021-10-17 18:48:23 +0700

    ss

--- index.php
+++ index.php
@@ -11,48 +11,48 @@ include 'include/header.php';
             </div>
             <div class="icon-dev">
                 <div class="box-one">
-                    <img src="/asset/logo/HTML.jpg" alt="">
-                    <img src="/asset/logo/CSS.jpg" alt="">
-                    <img src="/asset/logo/Laravel.jpg" alt="">
-                    <img src="/asset/logo/Flutter.jpg" alt="">
+                    <img src="asset/logo/HTML.jpg" alt="">
+                    <img src="asset/logo/CSS.jpg" alt="">
+                    <img src="asset/logo/Laravel.jpg" alt="">
+                    <img src="asset/logo/Flutter.jpg" alt="">
                 </div>
                 <div class="box-two">
-                    <img src="/asset/logo/JS.jpg" alt="">
-                    <img src="/asset/logo/MongoDB.jpg" alt="">
-                    <img src="/asset/logo/NodeJS.jpg" alt="">
-                    <img src="/asset/logo/PHP.jpg" alt="">
+                    <img src="asset/logo/JS.jpg" alt="">
+                    <img src="asset/logo/MongoDB.jpg" alt="">
+                    <img src="asset/logo/NodeJS.jpg" alt="">
+                    <img src="asset/logo/PHP.jpg" alt="">
                 </div>
                 <div class="box-three">
-                    <img src="/asset/logo/SQL.jpg" alt="">
-                    <img src="/asset/logo/Sublime text.jpg" alt="">
-                    <img src="/asset/logo/VS code.jpg" alt="">
-                    <img src="/asset/logo/NodeJSjpg.jpg" alt="">
+                    <img src="asset/logo/SQL.jpg" alt="">
+                    <img src="asset/logo/Sublime text.jpg" alt="">
+                    <img src="asset/logo/VS code.jpg" alt="">
+                    <img src="asset/logo/NodeJSjpg.jpg" alt="">
                 </div>
             </div>
         </div>
         <div class="container-right">
-            <img src="/asset/logo/computer.png" alt="">
+            <img src="asset/logo/computer.png" alt="">
         </div>
     </div>
     <div class="icon-contact">
         <a href="">
-            <img src="/asset/icon side bar/whatsapp.jpg" alt="">
+            <img src="asset/icon side bar/whatsapp.jpg" alt="">
         </a>
         <a href="">
-            <img src="/asset/icon side bar/facebook.png" alt="">
+            <img src="asset/icon side bar/facebook.png" alt="">
         </a>
         <a href="">
-            <img src="/asset/icon side bar/phone.jpg" alt="">
+            <img src="asset/icon side bar/phone.jpg" alt="">
         </a>
         <a href="">
-            <img src="/asset/icon side bar/githup.jpg" alt="">
+            <img src="asset/icon side bar/githup.jpg" alt="">
         </a>
     </div>
 </div>
 <div class="course">
     <div class="box-course">
         <div class="box-image">
-            <img src="/asset/logo/laravel-course.jpg" alt="">
+            <img src="asset/logo/laravel-course.jpg" alt="">
             <div class="get-start">
                 <a href="">ລົງທະບຽນ</a>
             </div>
@@ -68,7 +68,7 @@ include 'include/header.php';
     </div>
     <div class="box-course">
         <div class="box-image">
-            <img src="/asset/logo/laravel-course.jpg" alt="">
+            <img src="asset/logo/laravel-course.jpg" alt="">
             <div class="get-start">
                 <a href="">ລົງທະບຽນ</a>
             </div>
@@ -84,7 +84,7 @@ include 'include/header.php';
     </div>
     <div class="box-course">
         <div class="box-image">
-            <img src="/asset/logo/laravel-course.jpg" alt="">
+            <img src="asset/logo/laravel-course.jpg" alt="">
             <div class="get-start">
                 <a href="">ລົງທະບຽນ</a>
             </div>
@@ -100,7 +100,7 @@ include 'include/header.php';
     </div>
     <div class="box-course">
         <div class="box-image">
-            <img src="/asset/logo/laravel-course.jpg" alt="">
+            <img src="asset/logo/laravel-course.jpg" alt="">
             <div class="get-start">
                 <a href="">ລົງທະບຽນ</a>
             </div>
